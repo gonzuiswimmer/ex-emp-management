89c1ff605450a999ca15a6d11af7541fb9dfb2dd
gonzuiswimmer <gonzuiswimmer@gmail.com>
2024-02-07 09:45:39 +0900

    AdministratorService#insertの実装

--- src/main/java/com/example/service/AdministratorService.java
+++ src/main/java/com/example/service/AdministratorService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.domain.Administrator;
 import com.example.repository.AdministratorRepository;
 
 @Service
@@ -11,4 +12,9 @@ import com.example.repository.AdministratorRepository;
 public class AdministratorService {
   @Autowired
   private AdministratorRepository repository;
+
+  public void insert(Administrator admin){
+    repository.insert(admin);
+  }
+  
 }
