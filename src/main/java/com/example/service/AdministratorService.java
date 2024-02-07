fb6238204f10858377a4aeb942e9ba96a79552bc
gonzuiswimmer <gonzuiswimmer@gmail.com>
2024-02-07 10:10:42 +0900

    AdministratorService#loginの実装

--- src/main/java/com/example/service/AdministratorService.java
+++ src/main/java/com/example/service/AdministratorService.java
@@ -13,8 +13,24 @@ public class AdministratorService {
   @Autowired
   private AdministratorRepository repository;
 
+  /**
+   * 管理者情報の登録処理.
+   * 
+   * @param admin 管理者オブジェクト
+   * @return void
+   */
   public void insert(Administrator admin){
     repository.insert(admin);
   }
   
+  /**
+   * メールアドレスとパスワードをもとにログイン処理を行う.
+   * 
+   * @param mailAddress
+   * @param password
+   * @return Administrator 条件に合致した管理者オブジェクト
+   */
+  public Administrator login(String mailAddress, String password){
+    return repository.findByMailAddressAndPassword(mailAddress, password);
+  }
 }
