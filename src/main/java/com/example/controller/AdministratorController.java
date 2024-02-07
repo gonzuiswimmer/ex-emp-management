99c7ce4425f893ef5fa348ed84a4f1833d13e74f
gonzuiswimmer <gonzuiswimmer@gmail.com>
2024-02-07 09:49:20 +0900

    AdministratorController#insertの実装

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -1,12 +1,17 @@
 package com.example.controller;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.domain.Administrator;
 import com.example.form.InsertAdministratorForm;
 import com.example.service.AdministratorService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @Controller
@@ -25,5 +30,21 @@ public class AdministratorController {
   public String toInsert(InsertAdministratorForm form) {
     return "administrator/insert";
   }
+
+  /**
+   * 管理者情報を登録し、/ にリダイレクト.
+   * 
+   * @param form
+   * @return / リダイレクト
+   */
+  @PostMapping("/insert")
+  public String insert(InsertAdministratorForm form) {
+    Administrator admin = new Administrator();
+    BeanUtils.copyProperties(form, admin);
+
+    service.insert(admin);
+    return "redirect:/";
+  }
+  
   
 }
